e6e3a89bf1eb2ccfbf36292766fb984fda6e876a
Lorenz Pfeifer <pfeifer.lorenz@gmail.com>
2022-12-04 12:54:48 +0100

    :card_file_box: add department

---
+++ database/migrations/2022_12_04_114210_create_departments_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('departments_users', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->unsignedBigInteger('department_id');
+            $table->foreign('department_id')->references('id')->on('departments');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('departments_users');
+    }
+};
