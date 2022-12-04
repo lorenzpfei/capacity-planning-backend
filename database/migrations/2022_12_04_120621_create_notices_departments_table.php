5609452f6718c0606608729121b8390017017cc9
Lorenz Pfeifer <pfeifer.lorenz@gmail.com>
2022-12-04 13:09:53 +0100

    :card_file_box: add notice

---
+++ database/migrations/2022_12_04_120621_create_notices_departments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notices_departments', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->unsignedBigInteger('notice_id');
+            $table->foreign('notice_id')->references('id')->on('notices');
+            $table->unsignedBigInteger('department_id');
+            $table->foreign('department_id')->references('id')->on('departments');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notices_departments');
+    }
+};
