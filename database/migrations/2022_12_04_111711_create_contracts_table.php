340a003dd9de9346a0dc7e61b8b23fa28cd047ca
Lorenz Pfeifer <pfeifer.lorenz@gmail.com>
2022-12-04 12:30:51 +0100

    :card_file_box: add contract

---
+++ database/migrations/2022_12_04_111711_create_contracts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contracts', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamp('from');
+            $table->timestamp('to')->nullable();
+            $table->tinyInteger('hours_per_week');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contracts');
+    }
+};
