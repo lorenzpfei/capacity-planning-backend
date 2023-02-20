2c041a6dd287cb35036342ef690d53ff15170d27
Lorenz Pfeifer <pfeifer.lorenz@gmail.com>
2023-02-20 21:49:48 +0100

    :sparkles: move workload route to api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\WorkloadController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/me', function (Request $request) {
     return $request->user()->makeHidden(['login_token', 'login_refresh_token', 'task_user_id', 'task_token', 'task_refresh_token', 'tracking_user_id', 'tracking_refresh_token', 'tracking_token']);
 });
+
+Route::middleware('auth:sanctum')->controller(WorkloadController::class)->group(function () {
+    Route::get('/workload/{departmentId}/{from?}/{to?}', 'getWorkloadForDepartment');
+});
+
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
 use App\Http\Controllers\OAuthController;
-use App\Http\Controllers\WorkloadController;
-use App\Providers\TaskServiceProvider;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,15 +13,6 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', static function () {
-    return view('welcome');
-});
-
-Route::controller(WorkloadController::class)->group(function () {
-    Route::get('/workload/{departmentId}/{from?}/{to?}', 'getWorkloadForDepartment');
-    //Route::post('/workload', 'store');
-});
 
 Route::get('oauth/{provider}', [OAuthController::class, 'redirectToProvider']);
 Route::get('oauth/{provider}/callback', [OAuthController::class, 'handleProviderCallback']);
